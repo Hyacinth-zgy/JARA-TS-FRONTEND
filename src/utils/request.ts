8139e1ac0fd95e4e462b3174501414ba9d74c5e2
Hyacinth-zgy <838673371@qq.com>
2021-04-07 19:28:00 +0800

    封装requst请求

---
+++ src/utils/request.ts
@@ -0,0 +1,43 @@
+import * as auth from '../auth-provider';
+import qs from 'qs';
+import {useAuth} from '../context/auth-context';
+const apiURL = process.env.REACT_APP_API_URL;
+
+interface Config extends RequestInit {
+  token?: string;
+  data?: object;
+}
+
+export const http = async (
+  endpoint: string,
+  {data, token, headers, ...customConfig}: Config
+) => {
+  const config = {
+    method: 'GET',
+    headers: {
+      Authorization: token ? `Bearer ${token}` : '',
+      'Content-Type': data ? 'application/json' : '',
+    },
+    ...customConfig,
+  };
+  if (config.method.toUpperCase() === 'GET') {
+    endpoint += `?${qs.stringify(data)}`;
+  } else {
+    config.body = JSON.stringify(data || {});
+  }
+  return window
+    .fetch(`${apiURL}/${endpoint}`, config)
+    .then(async (response) => {
+      if (response.status === 401) {
+        await auth.logout();
+        window.location.reload();
+        return Promise.reject({message: '请重新登录'});
+      }
+      const data = await response.json();
+      if (response.ok) {
+        return data;
+      } else {
+        return Promise.reject(data);
+      }
+    });
+};
