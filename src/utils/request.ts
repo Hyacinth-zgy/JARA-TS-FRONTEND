2fd9e17cc3964cfb14382fdd37ce62278f646cfa
Hyacinth-zgy <838673371@qq.com>
2021-04-19 16:59:21 +0800

    delete no use

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,7 +1,6 @@
 import * as auth from '../auth-provider';
 import qs from 'qs';
 import {useAuth} from '../context/auth-context';
-import {type} from 'os';
 const apiURL = process.env.REACT_APP_API_URL;
 
 interface Config extends RequestInit {
