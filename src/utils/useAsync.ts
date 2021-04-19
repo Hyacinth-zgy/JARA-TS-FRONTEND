b87b229f1e251fa6daac8b29dacb0ae755035704
Hyacinth-zgy <838673371@qq.com>
2021-04-19 14:32:25 +0800

    完成useasync函数

--- src/utils/useAsync.ts
+++ src/utils/useAsync.ts
@@ -1,1 +1,58 @@
-export const useAsync = () => {};
+import {useState} from 'react';
+interface State<D> {
+  error: Error | null;
+  data: D | null;
+  stat: 'idle' | 'loading' | 'error' | 'success';
+}
+
+const defaultInitialState: State<null> = {
+  stat: 'idle',
+  data: null,
+  error: null,
+};
+export const useAsync = <D>(initialState?: State<D>) => {
+  const [state, setState] = useState<State<D>>({
+    ...defaultInitialState,
+    ...initialState,
+  });
+
+  const setData = (data: D) =>
+    setState({
+      data,
+      stat: 'success',
+      error: null,
+    });
+
+  const setError = (error: Error) =>
+    setState({
+      error,
+      stat: 'error',
+      data: null,
+    });
+
+  // 用来触发异步请求
+  const run = (promise: Promise<D>) => {
+    if (!promise || !promise.then) {
+      throw new Error('请传入一个Promise的数据');
+    }
+    return promise
+      .then((data) => {
+        setData(data);
+        return data;
+      })
+      .catch((err) => {
+        setError(err);
+        return err;
+      });
+  };
+
+  return {
+    isIdle: state.stat === 'idle',
+    isLoading: state.stat === 'loading',
+    isError: state.stat === 'error',
+    isSuccess: state.stat === 'success',
+    run,
+    setData,
+    setError,
+  };
+};
