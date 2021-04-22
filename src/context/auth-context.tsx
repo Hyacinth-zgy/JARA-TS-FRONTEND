e85571435c1c3f7bd142f28a223d3984c2a6aa8d
Hyacinth-zgy <838673371@qq.com>
2021-04-22 12:10:33 +0800

    配置加载失败页面

--- src/context/auth-context.tsx
+++ src/context/auth-context.tsx
@@ -1,8 +1,10 @@
 import React, {useState, ReactNode} from 'react';
+import {FullPageLoading, FullPageErrorFallback} from '../components/libs';
 import {AuthForm, User} from '../utils/interface';
 import * as AUTH from '../auth-provider';
 import {useMount} from '../utils/helper';
 import {http} from '../utils/request';
+import {useAsync} from '../utils/useAsync';
 const bootstrapUser = async () => {
   let user = null;
   const token = AUTH.getToken();
@@ -24,7 +26,17 @@ const AuthContext = React.createContext<
 AuthContext.displayName = 'AuthContext';
 
 export const AuthProvider = ({children}: {children: ReactNode}) => {
-  const [user, setUser] = useState<User | null>(null);
+  // 原来: const [user, setUser] = useState<User | null>(null);
+  // 改造:
+  const {
+    data: user,
+    error,
+    isLoading,
+    isIdle,
+    isError,
+    run,
+    setData: setUser,
+  } = useAsync<User | null>();
   const login = (form: AuthForm) =>
     AUTH.login(form).then((user) => {
       setUser(user);
@@ -39,8 +51,19 @@ export const AuthProvider = ({children}: {children: ReactNode}) => {
     });
   useMount(() => {
     // 初始化时获取user信息
-    bootstrapUser().then(setUser);
+    // 原:  bootstrapUser().then(setUser);
+    // 改造:
+    run(bootstrapUser());
   });
+
+  // 如果正在获取信息
+  if (isIdle || isLoading) {
+    return FullPageLoading();
+  }
+
+  if (isError) {
+    return <FullPageErrorFallback error={error} />;
+  }
   return (
     <AuthContext.Provider
       children={children}
