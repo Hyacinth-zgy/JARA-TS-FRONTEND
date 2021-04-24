9b6fb4020b16f1fb8086e7a2adafcbc1cc68e09a
Hyacinth-zgy <838673371@qq.com>
2021-04-24 15:39:28 +0800

    delete no significance code

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -5,7 +5,7 @@ export const AppProviders = ({children}: {children: ReactNode}) => {
   // return <AuthProvider children={children}></AuthProvider>; children属性和在内部写children是一致的
   return (
     <QueryClientProvider client={new QueryClient()}>
-      <AuthProvider>{children} </AuthProvider>;
+      <AuthProvider>{children} </AuthProvider>
     </QueryClientProvider>
   );
 };
