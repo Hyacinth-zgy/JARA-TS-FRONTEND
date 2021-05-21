428dbe294064c874f9e25dcccf04d9b9c46a49a3
Hyacinth-zgy <838673371@qq.com>
2021-05-21 11:14:02 +0800

    合并状态，改进useUndo

--- src/utils/use-undo.ts
+++ src/utils/use-undo.ts
@@ -1,1 +1,118 @@
-export const useUndo = <T>(initialPresent: T) => {};
+import {useState, useCallback} from 'react';
+
+export const useUndo = <T>(initialPresent: T) => {
+  const [past, setPast] = useState<T[]>([]);
+  const [present, setPresent] = useState(initialPresent);
+  const [future, setFuture] = useState<T[]>([]);
+  const canUndo = past.length !== 0;
+  const canRedo = future.length !== 0;
+
+  const undo = useCallback(() => {
+    if (!canUndo) return;
+    const previous = past[past.length - 1];
+    const newPast = past.slice(0, past.length - 1);
+    setPast(newPast);
+    setPresent(previous);
+    setFuture([present, ...future]);
+  }, [canUndo, future, past, present]);
+
+  const redo = useCallback(() => {
+    if (!canRedo) return;
+    const next = future[0];
+    const newFuture = future.slice(1);
+    setPast([...past, present]);
+    setPresent(next);
+    setFuture(newFuture);
+  }, [canRedo, future, past, present]);
+
+  const set = useCallback(
+    (newPresent: T) => {
+      if (newPresent === present) return;
+      setPast([...past, present]);
+      setPresent(newPresent);
+      setFuture([]);
+    },
+    [past, present]
+  );
+
+  const reset = useCallback((newPresent: T) => {
+    setPast([]);
+    setPresent(newPresent);
+    setFuture([]);
+  }, []);
+
+  return [
+    {past, present, future},
+    {set, reset, undo, redo, canUndo, canRedo},
+  ];
+};
+
+// 对于同一个HOOK中有好几个状态，但是这些状态又是相互影响的，这种情况我们应该把这几个状态合并在一起，这样做可以大大降低写HOOK的复杂程度
+// 改写useUndo，使用HOOK合并的方式
+
+export const useUndoHooksMerge = <T>(initialPresent: T) => {
+  const [state, setState] = useState<{
+    past: T[];
+    present: T;
+    future: T[];
+  }>({
+    past: [],
+    present: initialPresent,
+    future: [],
+  });
+  const canUndo = state.past.length !== 0;
+  const canRedo = state.future.length !== 0;
+  const undo = useCallback(() => {
+    setState((currentState) => {
+      const {past, present, future} = currentState;
+      if (past.length === 0) {
+        return currentState;
+      }
+      const previous = past[past.length - 1];
+      const newPast = past.slice(0, past.length - 1);
+      return {
+        past: newPast,
+        future: [present, ...future],
+        present: previous,
+      };
+    });
+  }, []);
+
+  const redo = useCallback(() => {
+    setState((currentState) => {
+      const {past, present, future} = currentState;
+      if (future.length === 0) return currentState;
+      const next = future[0];
+      const newFuture = future.slice(1);
+      return {
+        past: [...past, present],
+        present: next,
+        future: newFuture,
+      };
+    });
+  }, []);
+
+  const set = useCallback((newPresent: T) => {
+    setState((currentState) => {
+      const {past, present} = currentState;
+      if (newPresent === present) return currentState;
+      return {
+        past: [...past, present],
+        present: newPresent,
+        future: [],
+      };
+    });
+  }, []);
+
+  const reset = useCallback((newPresent: T) => {
+    setState((currentState) => {
+      return {
+        past: [],
+        present: newPresent,
+        future: [],
+      };
+    });
+  }, []);
+
+  return [state, {set, reset, undo, redo, canUndo, canRedo}];
+};
